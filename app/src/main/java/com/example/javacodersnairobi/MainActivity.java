9e7c09f1647d6ad2210ab0928dbe69cfbf8c7537
ongebo <isaacongebo@gmail.com>
2019-03-01 14:56:30 +0300

    feat(recycler-view): create layout file for RecyclerView container
    - Add RecyclerView dependency to build.gradle
    - Add RecyclerView layout file
    - Add layout file for Github user's avatar and username
    
    [Finishes #155738443]

--- app/src/main/java/com/example/javacodersnairobi/MainActivity.java
+++ app/src/main/java/com/example/javacodersnairobi/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.javacodersnairobi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.users_list);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
     }
 }
