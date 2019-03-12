8bbecb646c2f550f29dcd568de50c523acb641b0
ongebo <isaacongebo@gmail.com>
2019-03-12 10:25:35 +0300

    feat(profile-details): launch detail activity from the main activity
    - Add detail activity launch button to activity_main.xml
    - Define showProfileDetails() in the main activity class
    - Attach showProfileDetails() as event handler for the launch button
    - Specify main activity as the detail activity's parent
    
    [Finishes #155820624]

--- app/src/main/java/com/example/javacodersnairobi/MainActivity.java
+++ app/src/main/java/com/example/javacodersnairobi/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.javacodersnairobi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
@@ -17,5 +19,10 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setHasFixedSize(true);
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
+    }
+
+    public void showProfileDetails(View view) {
+        Intent intent = new Intent(this, DetailActivity.class);
+        startActivity(intent);
     }
 }
