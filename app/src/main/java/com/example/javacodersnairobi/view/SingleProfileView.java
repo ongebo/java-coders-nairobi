a2aee446ce922d97e25f83fb7705c157889fe50c
ongebo <isaacongebo@gmail.com>
2019-03-13 18:51:42 +0300

    feat(api-access): define presenter class to fetch required API data
    - Define method to return retrofit instance in GithubService.java
    - Add interface definitions of views to display fetched API data
    - Define methods in GithubPresenter.java to fetch required API data
    
    [Finishes #155931587]

---
+++ app/src/main/java/com/example/javacodersnairobi/view/SingleProfileView.java
@@ -0,0 +1,8 @@
+package com.example.javacodersnairobi.view;
+
+import com.example.javacodersnairobi.model.GithubUsers;
+
+public interface SingleProfileView {
+    public void displayProfile(GithubUsers profile);
+    public void displayError();
+}
