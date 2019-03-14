c79aa5938e0ee3e41f2ddbd619a49dbf4d87f0f0
ongebo <isaacongebo@gmail.com>
2019-03-14 15:51:19 +0300

    feat(adapter): create the github users adapter
    - Add Picasso dependency to app module's build.gradle
    - Update layout file which displays a single item in the users list
    - Define adapter for the recycler view in GithubAdapter.java
    - Correct the BASE_URL for the Github API
    
    [Finishes #155965773]

---
+++ app/src/main/java/com/example/javacodersnairobi/adapter/GithubAdapter.java
@@ -0,0 +1,55 @@
+package com.example.javacodersnairobi.adapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.javacodersnairobi.R;
+import com.example.javacodersnairobi.model.GithubUsers;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class GithubAdapter extends RecyclerView.Adapter<GithubAdapter.GithubUsersViewHolder> {
+    private List<GithubUsers> dataSet;
+    private Context context;
+
+    public static class GithubUsersViewHolder extends RecyclerView.ViewHolder {
+        public View view;
+
+        public GithubUsersViewHolder(View view) {
+            super(view);
+            this.view = view;
+        }
+    }
+
+    public GithubAdapter(List<GithubUsers> data, Activity parent) {
+        dataSet = data;
+        context = parent;
+    }
+
+    @Override
+    public GithubUsersViewHolder onCreateViewHolder(ViewGroup parent, int type) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.view_item, parent, false);
+        return new GithubUsersViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(GithubUsersViewHolder holder, int position) {
+        TextView textView = holder.view.findViewById(R.id.username);
+        ImageView imageView = holder.view.findViewById(R.id.profile_image);
+        textView.setText(dataSet.get(position).getUsername());
+        Picasso.with(context).load(dataSet.get(position).getAvatar()).into(imageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataSet.size();
+    }
+}
