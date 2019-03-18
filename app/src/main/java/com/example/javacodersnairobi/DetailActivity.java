d70d192fb4d10823ea49e036302bfa70bbb7eb88
ongebo <isaacongebo@gmail.com>
2019-03-18 16:28:32 +0300

    feat(share): enable sharing of the displayed user profiles
    - Attach on-click listener to share button in DetailActivity.java
    - Update GithubUsers.java to obtain a user's profile url
    
    [Finishes #156008359]

--- app/src/main/java/com/example/javacodersnairobi/DetailActivity.java
+++ app/src/main/java/com/example/javacodersnairobi/DetailActivity.java
@@ -1,9 +1,11 @@
 package com.example.javacodersnairobi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -25,6 +27,8 @@ public class DetailActivity extends AppCompatActivity implements SingleProfileVi
     @Override
     public void displayProfile(GithubUsers profile) {
         View view = findViewById(R.id.detail_layout);
+        final String profileUrl = profile.getProfileUrl();
+        final String githubHandle = profile.getUsername();
 
         // Obtain references to views on the profile details page
         ImageView profileImage = view.findViewById(R.id.profile_image);
@@ -33,6 +37,7 @@ public class DetailActivity extends AppCompatActivity implements SingleProfileVi
         TextView repos = view.findViewById(R.id.repos);
         TextView followers = view.findViewById(R.id.followers);
         TextView following = view.findViewById(R.id.following);
+        Button shareButton = view.findViewById(R.id.button);
 
         // Assign values to the referenced views
         Picasso.with(this).load(profile.getAvatar()).into(profileImage);
@@ -41,6 +46,17 @@ public class DetailActivity extends AppCompatActivity implements SingleProfileVi
         repos.setText(profile.getRepos());
         followers.setText(profile.getFollowers());
         following.setText(profile.getFollowing());
+        shareButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                String shareMessage = "Checkout this awesome developer @" + githubHandle + ", " +
+                        profileUrl + ".";
+                intent.putExtra(Intent.EXTRA_TEXT, shareMessage);
+                startActivity(Intent.createChooser(intent, "Share Github profile with:"));
+            }
+        });
     }
 
     @Override
